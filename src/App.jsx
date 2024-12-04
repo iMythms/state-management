12dc334a5fa2b7f05c32b79f6789873bb0dd60e3
iMythms <maythams.149@gmail.com>
2024-12-04 20:33:49 +0300

    feat: integrate Tailwind CSS and implement dark mode toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 const App = () => {
+	const [darkMode, setDarkMode] = useState(false)
+	const [userInfo, setUserInfo] = useState({
+		firstName: 'Mytham',
+		lastName: 'Jasim',
+		hasPets: true,
+		age: 24,
+	})
+
+	const toggleDarkMode = () => {
+		setDarkMode(!darkMode)
+		console.log(`Dark Mode is ${darkMode}!`)
+	}
+
 	return (
-		<div>
-			<h1>Hello World!</h1>
-		</div>
+		<section className="container mx-auto">
+			<div className={darkMode ? 'dark' : 'light'}>
+				<h1>Hello World</h1>
+				<p>
+					Hi! My name is {userInfo.firstName} {userInfo.lastName}, I am{' '}
+					{userInfo.age} years old and I do {userInfo.hasPets ? '' : 'not'} have
+					pets.
+				</p>
+				<button
+					onClick={() => {
+						toggleDarkMode()
+					}}
+				>
+					{darkMode ? 'Light' : 'Dark'}
+				</button>
+			</div>
+		</section>
 	)
 }
 
